5e1c6d02676296bdc2912c0c4adcfdef822fde07
Roman Teplyakov <kullermami@yandex.ru>
2023-12-26 19:29:40 +0300

    docs: added classes.py, constants.py, main.py, level_1.txt, data(box.png, bricks.png, mario.png)

---
+++ main.py
@@ -0,0 +1,109 @@
+import os
+import sys
+import pygame
+
+from constants import WIDTH, HEIGHT, FPS, STEP
+
+from classes import Player, Tile, Camera
+
+pygame.init()
+pygame.key.set_repeat(200, 70)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+player = None
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+tile_images = {'brick': load_image('bricks.png'),
+               'box': load_image('box.png')}
+player_image = load_image('mario.png')
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '#':
+                Tile(tiles_group, all_sprites, 'brick', tile_images,
+                     x, y)
+            elif level[y][x] == '@':
+                new_player = Player(player_group, all_sprites, player_image,
+                                    5, x, y)
+            elif level[y][x] == ':':
+                Tile(tiles_group, all_sprites, 'box', tile_images,
+                     x, y)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+player, level_x, level_y = generate_level(load_level("level_1.txt"))
+camera = Camera((level_x, level_y))
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                player.rect.x -= STEP
+            if event.key == pygame.K_d:
+                player.rect.x += STEP
+            if event.key == pygame.K_SPACE:
+                player.rect.y -= STEP
+
+    camera.update(player)
+
+    for sprite in all_sprites:
+        camera.apply(sprite)
+
+    screen.fill(pygame.Color(89, 151, 254))
+    tiles_group.draw(screen)
+    player_group.draw(screen)
+
+    pygame.display.flip()
+
+    clock.tick(FPS)
+
+terminate()
