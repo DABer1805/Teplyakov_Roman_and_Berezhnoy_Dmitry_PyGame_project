139283bb9d1a73c0bf5b4f25354a29b201029788
Roman Teplyakov <kullermami@yandex.ru>
2023-12-28 02:14:26 +0300

    refactor: class Tile feat: class Wall, class Box, class Player rebase onto class Entity, refactor Camera.update(), deleted class Entity style: deleted all unnecessary comments

--- classes.py
+++ classes.py
@@ -4,55 +4,48 @@ from constants import TILE_WIDTH, TILE_HEIGHT, WIDTH, HEIGHT
 
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, tiles_group, all_sprites, tile_type,
-                 tile_images, pos_x, pos_y):
+    def __init__(self, tiles_group, all_sprites):
         super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
+
+
+class Wall(Tile):
+    def __init__(self, tiles_group, all_sprites, tile_images, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images['brick']
         self.rect = self.image.get_rect().move(TILE_WIDTH * pos_x,
                                                TILE_HEIGHT * pos_y)
 
 
+class Box(Tile):
+    def __init__(self, tiles_group, all_sprites, tile_images, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images['box']
+        self.rect = self.image.get_rect().move(TILE_WIDTH * pos_x,
+                                               TILE_HEIGHT * pos_y)
+        self.hp = 50
+
+
 class Camera:
-    # зададим начальный сдвиг камеры и размер поля для возможности реализации циклического сдвига
     def __init__(self, field_size):
         self.dx = 0
         self.dy = 0
         self.field_size = (field_size[0], field_size[1] - 1)
 
-    # сдвинуть объект obj на смещение камеры
     def apply(self, obj):
         obj.rect.x += self.dx
-        # вычислим координату клитки, если она уехала влево за границу экрана
-        if obj.rect.x < -obj.rect.width:
-            obj.rect.x += (self.field_size[0] + 1) * obj.rect.width
-        # вычислим координату клитки, если она уехала вправо за границу экрана
-        if obj.rect.x >= (self.field_size[0]) * obj.rect.width:
-            obj.rect.x += -obj.rect.width * (1 + self.field_size[0])
         obj.rect.y += self.dy
-        # вычислим координату клитки, если она уехала вверх за границу экрана
-        if obj.rect.y < -obj.rect.height:
-            obj.rect.y += (self.field_size[1] + 1) * obj.rect.height
-        # вычислим координату клитки, если она уехала вниз за границу экрана
-        if obj.rect.y >= (self.field_size[1]) * obj.rect.height:
-            obj.rect.y += -obj.rect.height * (1 + self.field_size[1])
 
-    # позиционировать камеру на объекте target
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
+    def update(self, target, tiles_group):
+        if not pygame.sprite.spritecollideany(target, tiles_group):
+            self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
+            self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
 
 
-class Entity(pygame.sprite.Sprite):
-    def __init__(self, entity_group, all_sprites,
-                 entity_image, pos_x, pos_y):
-        super().__init__(entity_group, all_sprites)
-        self.image = entity_image
-        self.rect = self.image.get_rect().move(TILE_WIDTH * pos_x + 15,
-                                               TILE_HEIGHT * pos_y + 5)
-
-class Player(Entity):
+class Player(pygame.sprite.Sprite):
     def __init__(self, player_group, all_sprites, player_image, hp,
                  pos_x, pos_y):
-        super().__init__(player_group, all_sprites, player_image,
-                         pos_x, pos_y)
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(TILE_WIDTH * pos_x + 15,
+                                               TILE_HEIGHT * pos_y + 5)
         self.hp = hp
